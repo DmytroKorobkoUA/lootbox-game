f630d448190e520560238d7b916ed4f8587363b7
DmytroKorobkoUA <dmytro.korobko@devabit.com>
2024-08-25 23:05:03 +0100

    Separate sockets logic from the server

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const playerRoutes = require('./routes/playerRoutes');
 const lootboxRoutes = require('./routes/lootboxRoutes');
 const rewardRoutes = require('./routes/rewardRoutes');
-const Lootbox = require('./models/lootbox');
 const cors = require('cors');
 const { Server } = require('socket.io');
 const http = require('http');
@@ -18,7 +17,7 @@ const io = new Server(server, {
     }
 });
 
-const PORT = process.env.PORT || 3000;
+const setupSockets = require('./sockets/gameSockets');
 
 const corsOptions = {
     origin: process.env.FRONTEND_URL,
@@ -41,122 +40,13 @@ mongoose.connect(process.env.MONGO_URI, {
 app.get('/', (req, res) => {
     res.send('Loot box game server is running');
 });
-
 app.use('/api/players', playerRoutes);
 app.use('/api/lootboxes', lootboxRoutes);
 app.use('/api/rewards', rewardRoutes);
 
-let gameInitiated = false;
-let gameStarted = false;
-let lootboxes = [];
-let countdownTimer;
-let countdownValue = 5;
-let playersInGame = [];
-let openedLootboxes = [];
-let gameLog = [];
+setupSockets(io);
 
-io.on('connection', (socket) => {
-    console.log('user connected', socket.id);
-
-    socket.on('disconnect', async () => {
-        console.log('user disconnected', socket.id);
-
-        const connectedSockets = await io.fetchSockets();
-
-        if (connectedSockets.length === 0) {
-            gameStarted = false;
-            gameInitiated = false;
-            playersInGame = [];
-            clearInterval(countdownTimer);
-            await Lootbox.deleteMany({});
-            io.emit('gameEnded');
-        }
-    });
-
-    socket.on('initGame', async ({ username }) => {
-        if (!gameInitiated) {
-            gameInitiated = true;
-            countdownValue = 5;
-            playersInGame = [username];
-            io.emit('playersInGameUpdate', playersInGame);
-            io.emit('gameInitiated', { countdown: countdownValue });
-
-            countdownTimer = setInterval(async () => {
-                countdownValue -= 1;
-                io.emit('gameInitiated', { countdown: countdownValue });
-
-                if (countdownValue <= 0) {
-                    clearInterval(countdownTimer);
-                    if (playersInGame.length > 1) {
-                        gameStarted = true;
-                        lootboxes = await Lootbox.find().sort({ createdAt: -1 }).limit(25);
-                        lootboxes = lootboxes.sort(() => Math.random() - 0.5);
-                        openedLootboxes = [];
-                        gameLog = [];
-                        io.emit('gameStarted', { lootboxes });
-                    } else {
-                        gameInitiated = false;
-                        playersInGame = [];
-                        io.emit('playersInGameUpdate', playersInGame);
-                    }
-                }
-            }, 1000);
-        }
-    });
-
-    socket.on('joinGame', async ({username}) => {
-        if (!playersInGame.includes(username)) {
-            playersInGame.push(username);
-            io.emit('playersInGameUpdate', playersInGame);
-        }
-
-        if (playersInGame.length >= 5) {
-            clearInterval(countdownTimer);
-            gameStarted = true;
-            gameInitiated = true;
-            lootboxes = await Lootbox.find();
-            lootboxes = lootboxes.sort(() => Math.random() - 0.5);
-            openedLootboxes = [];
-            gameLog = [];
-            io.emit('gameStarted', {lootboxes});
-        }
-    });
-
-    socket.on('lootboxOpened', async ({lootboxId, reward, imagePath, username, rarity}) => {
-        openedLootboxes.push(lootboxId);
-        gameLog.push(`Player ${username} opened ${rarity} box and got ${reward}`);
-        io.emit('lootboxOpened', {lootboxId, reward, imagePath});
-        io.emit('gameLogUpdate', gameLog);
-
-        if (openedLootboxes.length >= 25) {
-            gameStarted = false;
-            gameInitiated = false;
-            gameLog = [];
-            playersInGame = [];
-            clearInterval(countdownTimer);
-            await Lootbox.deleteMany({});
-            io.emit('gameEnded');
-        }
-    });
-
-    socket.on('endGame', async ({ username }) => {
-        playersInGame = playersInGame.filter(player => player !== username);
-        io.emit('playersInGameUpdate', playersInGame);
-
-        if (playersInGame.length === 0) {
-            gameStarted = false;
-            gameInitiated = false;
-            gameLog = [];
-            clearInterval(countdownTimer);
-            await Lootbox.deleteMany({});
-            io.emit('gameEnded');
-        } else {
-            if (!playersInGame.includes(username)) {
-                socket.emit('gameEnded');
-            }
-        }
-    });
-});
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
---
+++ backend/sockets/gameSockets.js
@@ -0,0 +1,115 @@
+const Lootbox = require('../models/lootbox');
+
+let gameInitiated = false;
+let gameStarted = false;
+let lootboxes = [];
+let countdownTimer;
+let countdownValue = 5;
+let playersInGame = [];
+let openedLootboxes = [];
+let gameLog = [];
+
+module.exports = (io) => {
+    io.on('connection', (socket) => {
+        console.log('user connected', socket.id);
+
+        socket.on('disconnect', async () => {
+            console.log('user disconnected', socket.id);
+
+            const connectedSockets = await io.fetchSockets();
+
+            if (connectedSockets.length === 0) {
+                gameStarted = false;
+                gameInitiated = false;
+                playersInGame = [];
+                clearInterval(countdownTimer);
+                await Lootbox.deleteMany({});
+                io.emit('gameEnded');
+            }
+        });
+
+        socket.on('initGame', async ({ username }) => {
+            if (!gameInitiated) {
+                gameInitiated = true;
+                countdownValue = 5;
+                playersInGame = [username];
+                io.emit('playersInGameUpdate', playersInGame);
+                io.emit('gameInitiated', { countdown: countdownValue });
+
+                countdownTimer = setInterval(async () => {
+                    countdownValue -= 1;
+                    io.emit('gameInitiated', { countdown: countdownValue });
+
+                    if (countdownValue <= 0) {
+                        clearInterval(countdownTimer);
+                        if (playersInGame.length > 1) {
+                            gameStarted = true;
+                            lootboxes = await Lootbox.find().sort({ createdAt: -1 }).limit(25);
+                            lootboxes = lootboxes.sort(() => Math.random() - 0.5);
+                            openedLootboxes = [];
+                            gameLog = [];
+                            io.emit('gameStarted', { lootboxes });
+                        } else {
+                            gameInitiated = false;
+                            playersInGame = [];
+                            io.emit('playersInGameUpdate', playersInGame);
+                        }
+                    }
+                }, 1000);
+            }
+        });
+
+        socket.on('joinGame', async ({ username }) => {
+            if (!playersInGame.includes(username)) {
+                playersInGame.push(username);
+                io.emit('playersInGameUpdate', playersInGame);
+            }
+
+            if (playersInGame.length >= 5) {
+                clearInterval(countdownTimer);
+                gameStarted = true;
+                gameInitiated = true;
+                lootboxes = await Lootbox.find();
+                lootboxes = lootboxes.sort(() => Math.random() - 0.5);
+                openedLootboxes = [];
+                gameLog = [];
+                io.emit('gameStarted', { lootboxes });
+            }
+        });
+
+        socket.on('lootboxOpened', async ({ lootboxId, reward, imagePath, username, rarity }) => {
+            openedLootboxes.push(lootboxId);
+            gameLog.push(`Player ${username} opened ${rarity} box and got ${reward}`);
+            io.emit('lootboxOpened', { lootboxId, reward, imagePath });
+            io.emit('gameLogUpdate', gameLog);
+
+            if (openedLootboxes.length >= 25) {
+                gameStarted = false;
+                gameInitiated = false;
+                gameLog = [];
+                playersInGame = [];
+                clearInterval(countdownTimer);
+                await Lootbox.deleteMany({});
+                io.emit('gameEnded');
+            }
+        });
+
+        socket.on('endGame', async ({ username }) => {
+            playersInGame = playersInGame.filter(player => player !== username);
+            io.emit('playersInGameUpdate', playersInGame);
+
+            if (playersInGame.length === 0) {
+                gameStarted = false;
+                gameInitiated = false;
+                gameLog = [];
+                clearInterval(countdownTimer);
+                await Lootbox.deleteMany({});
+                io.emit('gameEnded');
+            } else {
+                if (!playersInGame.includes(username)) {
+                    socket.emit('gameEnded');
+                }
+            }
+        });
+    });
+};
